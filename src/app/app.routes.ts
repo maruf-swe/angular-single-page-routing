73a06158f77207f7ebfbec7e2799763773c790b4
maruf-swe <marufur1847@gmail.com>
2024-01-06 01:09:40 +0600

    add redirect route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
 
 export const routes: Routes = [
+    {path: '', redirectTo: '/heroes-list', pathMatch: 'full'},
     {path:'crisis-list', component: CrisisListComponent},
     {path: 'heroes-list', component: HeroesListComponent}
 ];
