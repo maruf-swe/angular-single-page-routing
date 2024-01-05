2898f42c4d1725a3e0e259f486537451b3723438
maruf-swe <marufur1847@gmail.com>
2024-01-05 23:54:56 +0600

    import two componenet in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, CrisisListComponent, HeroesListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
