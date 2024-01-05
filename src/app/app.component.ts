d173193d4dda3f91ad5fcd0cb7ce8df261823690
maruf-swe <marufur1847@gmail.com>
2024-01-06 04:38:04 +0600

    modified app componenet & routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CrisisListComponent, HeroesListComponent, 
-    RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterOutlet, CrisisListComponent, HeroesListComponent,
+    PageNotFoundComponent, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
