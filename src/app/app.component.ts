8abb133b2df9972c7ddd1bfce9415bfe7dc8405a
maruf-swe <marufur1847@gmail.com>
2024-01-06 01:01:06 +0600

    add route to component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { CrisisListComponent } from './crisis-list/crisis-list.component';
 import { HeroesListComponent } from './heroes-list/heroes-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, CrisisListComponent, HeroesListComponent],
+  imports: [CommonModule, RouterOutlet, CrisisListComponent, HeroesListComponent, 
+    RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { CrisisListComponent } from './crisis-list/crisis-list.component';
+import { HeroesListComponent } from './heroes-list/heroes-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'crisis-list', component: CrisisListComponent},
+    {path: 'heroes-list', component: HeroesListComponent}
+];
